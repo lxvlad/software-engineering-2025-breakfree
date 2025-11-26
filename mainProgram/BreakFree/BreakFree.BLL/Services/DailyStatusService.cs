9e4b87f82365611ab83758adb210e31788365edb
TarasAndreiko <TARAS.ANDREIKO@lnu.edu.ua>
2025-11-26 03:52:41 +0200

    BreakFree-2: Implement unittests for services

--- mainProgram/BreakFree/BreakFree.BLL/Services/DailyStatusService.cs
+++ mainProgram/BreakFree/BreakFree.BLL/Services/DailyStatusService.cs
@@ -1,44 +1,40 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-using BreakFree.BLL.Interfaces;
+﻿using BreakFree.BLL.Interfaces;
+using BreakFree.DAL;
 using BreakFree.DAL.Entities;
 using BreakFree.DAL.Repositories;
+using System;
 using System.Collections.Generic;
-
 
 namespace BreakFree.BLL.Services
 {
     public class DailyStatusService : IDailyStatusService
     {
-        
         private readonly DailyStatusRepository _dailyStatusRepository;
 
-
-        public DailyStatusService()
+        // Порожній конструктор для продакшн
+        public DailyStatusService() : this(new DailyStatusRepository(new BreakFreeContext()))
         {
-            _dailyStatusRepository = new DailyStatusRepository();
         }
 
+        // Конструктор для тестів
+        public DailyStatusService(DailyStatusRepository dailyStatusRepository)
+        {
+            _dailyStatusRepository = dailyStatusRepository;
+        }
 
         public void AddDailyStatus(DailyStatus status)
         {
             _dailyStatusRepository.AddDailyStatus(status);
         }
 
-
         public List<DailyStatus> GetStatusesByHabit(int habitId)
         {
             return _dailyStatusRepository.GetStatusesByHabit(habitId);
         }
-
 
         public List<DailyStatus> GetStatusesByUser(int userId)
         {
             return _dailyStatusRepository.GetStatusesByUser(userId);
         }
     }
-}   
+}
--- mainProgram/BreakFree/BreakFree.DAL/Repositories/DailyStatusRepository.cs
+++ mainProgram/BreakFree/BreakFree.DAL/Repositories/DailyStatusRepository.cs
@@ -1,56 +1,50 @@
-﻿using System;
-using System.Collections.Generic;
-using BreakFree.DAL.Entities;
+﻿using BreakFree.DAL.Entities;
 using System.Collections.Generic;
 using System.Linq;
-
 
 namespace BreakFree.DAL.Repositories
 {
     public class DailyStatusRepository
     {
-        public void AddDailyStatus(DailyStatus status)
+        private readonly BreakFreeContext _context;
+
+        public DailyStatusRepository(BreakFreeContext context)
         {
-            using var context = new BreakFreeContext();
-            context.DailyStatuses.Add(status);
-            context.SaveChanges();
+            _context = context;
         }
 
+        public void AddDailyStatus(DailyStatus status)
+        {
+            _context.DailyStatuses.Add(status);
+            _context.SaveChanges();
+        }
 
         public List<DailyStatus> GetStatusesByHabit(int habitId)
         {
-            using var context = new BreakFreeContext();
-            return context.DailyStatuses.Where(s => s.HabitId == habitId).ToList();
+            return _context.DailyStatuses.Where(s => s.HabitId == habitId).ToList();
         }
-
 
         public void UpdateDailyStatus(DailyStatus status)
         {
-            using var context = new BreakFreeContext();
-            context.DailyStatuses.Update(status);
-            context.SaveChanges();
+            _context.DailyStatuses.Update(status);
+            _context.SaveChanges();
         }
-
 
         public void DeleteDailyStatus(int statusId)
         {
-            using var context = new BreakFreeContext();
-            var status = context.DailyStatuses.FirstOrDefault(s => s.StatusId == statusId);
-
+            var status = _context.DailyStatuses.FirstOrDefault(s => s.StatusId == statusId);
             if (status != null)
             {
-                context.DailyStatuses.Remove(status);
-                context.SaveChanges();
+                _context.DailyStatuses.Remove(status);
+                _context.SaveChanges();
             }
         }
 
-
         public List<DailyStatus> GetStatusesByUser(int userId)
         {
-            using var context = new BreakFreeContext();
-            return context.DailyStatuses
-                        .Where(s => context.Habits.Any(h => h.HabitId == s.HabitId && h.UserId == userId))
-                        .ToList();
+            return _context.DailyStatuses
+                .Where(s => _context.Habits.Any(h => h.HabitId == s.HabitId && h.UserId == userId))
+                .ToList();
         }
     }
 }
